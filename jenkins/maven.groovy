4735d0d5e512b9092e5dfe2c45fe18b8a26033eb
Mubarak <mubarakkhan13122000@gmail.com>
2023-04-15 11:42:24 +0530

    groovy

---
+++ jenkins/maven.groovy
@@ -0,0 +1,15 @@
+Job('maven_dsl') {
+    description{"first maven project"}
+    scm {
+        git("https://github.com/Mubarak17/jenkins.git", master)
+    }
+    triggers {
+        scm('* * * * *')
+    }
+    steps {
+        maven('clean pacakage', 'jenkins/pom.xml')
+    }
+    publishers {
+        archiveArtifacts '**/*.jar'
+    }
+}
